b429d59a5bdf4a9379a30d124c010904a9301620
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-04-20 00:27:02 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -4,75 +4,71 @@
 #include <random>
 #include "textgen.h"
 
-TextGen::TextGen() {}
+namespace std {
+    TextGen::TextGen() {}
 
-void TextGen::makeTab(std::string text, int count) {
-    std::vector<std::string> words;
-    char sep = ' ';
-    int start = 0;
-    int end = 0;
-    std::string t;
+    void TextGen::makeTab(string text, int count) {
+        string* words{new string[count]};
+        char sep = ' ';
+        int start = 0;
+        int end = 0;
+        string t;
 
-    for (int i = 0; i <= text.size(); i++) {
-        if (text[i] == sep || i == text.size()) {
-            end = i;
-            t.append(text, start, end - start);
-            words.push_back(t);
-            start = end + 1;
+        for (int i = 0; i <= text.size(); i++) {
+            if (text[i] == sep || i == text.size()) {
+                end = i;
+                t.append(text, start, end - start);
+                words->append(t);
+                start = end + 1;
+            }
+        }
+
+        first.push_back(words[0]);
+        first.push_back(words[1]);
+        
+        for (int i = 0; i < words->size() - count; i++) {
+            prefix pref;
+            for (int j = 0; j < count; j++) {
+                pref.push_back(words[i + j]);
+            }
+            statetab[pref].push_back(words[i + count]);
         }
     }
 
-    first.push_back(words[0]);
-    first.push_back(words[1]);
-
-    for (int i = 0; i < words.size() - count; i++) {
+    string TextGen::generateText(int count, unsigned int time_count) {
         prefix pref;
-        for (int j = 0; j < count; j++) {
-            pref.push_back(words[i + j]);
+        string text;
+        pref = first;
+
+        for (const string& elem : pref) {
+            text += elem + ' ';
         }
-        statetab[pref].push_back(words[i + count]);
-    }
-}
 
-std::string TextGen::generateText(int count, unsigned int time_count) {
-    prefix pref;
-    std::string text;
-    pref = first;
+        unsigned int a = time(0);
+        if (time_count != 0) {
+            a = time_count;
+        }
+        default_random_engine gen(a);
 
-    for (const std::string& elem : pref) {
-        text += elem + ' ';
-    }
+        while (text.size() < count) {
+            if (statetab.find(pref) != statetab.end()) {
+                const vector<string>& suffixes = statetab[pref];
 
-    unsigned int a = time(0);
-    if (time_count != 0) {
-        a = time_count;
-    }
-    std::default_random_engine gen(a);
-    while (text.size() < count) {
-        if (statetab.find(pref) != statetab.end()) {
-            const std::vector<std::string>& suffixes = statetab[pref];
-
-            if (!suffixes.empty()) {
-                std::uniform_int_distribution<> dist(0, suffixes.size() - 1);
-                int index = dist(gen);
-                text += suffixes[index] + ' ';
-                pref.pop_front();
-                pref.push_back(suffixes[index]);
+                if (!suffixes.empty()) {
+                    std::uniform_int_distribution<> dist(0, suffixes.size() - 1);
+                    int index = dist(gen);
+                    text += suffixes[index] + ' ';
+                    pref.pop_front();
+                    pref.push_back(suffixes[index]);
+                }
+                else {
+                    break;
+                }
             } else {
                 break;
             }
-        } else {
-            break;
         }
-    }
 
-    if (!text.empty() && text.back() == ' ') {
-        text.pop_back();
+        return text;
     }
-
-    if (text.size() > count) {
-        text = text.substr(0, count);
-    }
-
-    return text;
 }
