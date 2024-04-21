5f5c6300cc900a2f6803277dfe9718b6b708098d
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-04-21 22:18:21 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,53 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
+#include "textgen.h"
+
+TEST(test1, prefix) {
+  TextGen textgen;
+  std::vector<std::string>words{ "I", "am", "student"};
+  std::deque<std::string> prefix;
+  prefix.push_back("I");
+  prefix.push_back("am");
+  textgen.makeTab(words, 2);
+  EXPECT_EQ(prefix, mark.first);
+}
+
+TEST(test2, prefix_suffix) {
+    std::vector<std::string>words{ "I", "am", "good", "and", "kind", "student"};
+    TextGen textgen;
+    std::deque<std::string> prefix{ "I", "am" };
+    textgen.makeTab(words, 2);
+    std::vector<std::string> suffix{ "student" };
+    EXPECT_EQ(mark.statetab.begin()->second, suffix);
+}
+
+TEST(test3, suffix) {
+    std::vector<std::string>words{ "I", "am", "good", "and", "kind", "student" };
+    TextGen textgen;
+    textgen.makeTab(words, 1);
+    EXPECT_EQ(textgen.generateText(10, 10), "I am good and kind student");
+}
+
+TEST(test4, chooseSuffix) {
+    std::vector<std::string>words{ "a",
+    "b", "c", "d", "e",
+    "b", "c", "a", "b", "d", "e", "a"};
+    TextGen textgen;
+    textgen.makeTab(words, 1);
+    EXPECT_EQ(textgen.generateText(10, 10), "a b d e b d e a b c ");
+}
+
+TEST(test5, text) {
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > state = {
+        { { "a" }, { "A", "a" } },
+        { { "b" }, { "B", "b" } },
+        { {"c"}, {"C"} }
+    };
+    TextGen textgen;
+    typedef std::deque<std::string> prefix;
+    prefix pre{ "a" };
+    mark.statetab = state;
+    mark.first = pref;
+    EXPECT_EQ(textgen.generateText(10, 10), "b B ");
+}
